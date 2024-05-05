ce5bb82aee7f554a62f7bc47829c28a1816b37ee
Caio Schmidt <caioschmidtcdc@gmail.com>
2024-05-05 19:56:12 -0300

    Alterações tela cadastro e tela login

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-    <header className="h-[150px] bg-white drop-shadow-lg">
+  <div className="font-Inter">
+    <header className="h-[130px] bg-white drop-shadow-lg">
       <div className="flex justify-between items-center">
         <div className="pl-8">
-          <img src="./assets/logo.png" alt="logo prefeitura" className="h-[140px]"></img>
+          <img src="./assets/logo.png" alt="logo prefeitura" className="h-[130px]"></img>
         </div>
 
         <div className="flex flex-row gap-10 pr-16">
@@ -23,49 +23,28 @@ function App() {
       </div>
     </header>
 
-    <div className="bg-slate-300 w-full flex justify-center items-center pt-10">
-      <div className="bg-white w-3/4 rounded-sm flex flex-col items-center drop-shadow-lg mb-5">
-        <h1 className="text-center text-xl pt-7 pb-5 font-bold">Cadastre-se na Secretaria da Educação</h1>
+    <div className="bg-slate-300 w-full h-full flex flex-row gap-20 justify-center items-center pt-5 pb-[60px]">
+      <div className="w-[25rem] h-[28rem] bg-[#D9D9D9] drop-shadow-lg ">
+      </div>
 
-        <div className="grid grid-cols-2 gap-14 pl-20 pr-20 pb-10">
-          <div>
-            <p className="text-[#074173] pl-4 pb-1">Nome Completo</p>
-            <input type='textarea' placeholder='Informe seu nome completo' className='w-96 text-[#878787] rounded-md border border-gray-300 py-2 pl-4'></input>
+      <div className= "w-[25rem] h-[28rem] bg-white drop-shadow-lg px-5 flex flex-col">
+          <h1 className="text-center pt-7 text-xl pb-7">Indentifique-se na Secretaria de Educação</h1>
+          <p className="text-[#074173] pl-8 text-lg">E-mail ou Matricula</p>
+          <div className="flex flex-wrap justify-center items-center pb-7">
+            <input type='textarea' placeholder='Digite seu e-mail ou matricula' className='max-w-full w-80 text-[#878787] rounded-md border border-gray-500 py-3 pl-4'></input>
           </div>
-          <div>
-            <p className="text-[#074173] pl-4 pb-1">Matrícula</p>
-            <input type='textarea' placeholder='Formato (000000-0)' className='w-96 text-[#878787] rounded-md border border-gray-300 py-2 pl-4'></input>
+          <p className="text-[#074173] pl-8 text-lg">Senha</p>
+          <div className="flex flex-wrap justify-center items-center">
+            <input type='textarea' placeholder='Digite sua senha' className='max-w-full w-80 text-[#878787] rounded-md border border-gray-500 py-3 pl-4'></input>
           </div>
-          <div>
-            <p className="text-[#074173] pl-4 pb-1">E-mail</p>
-            <input type='textarea' placeholder='Informe seu e-mail' className='w-96 text-[#878787] rounded-md border border-gray-300 py-2 pl-4'></input>
+          <div className="flex flex-col content-center justify-center gap-5">
+            <button className="text-center pt-2 text-[#074173] hover:text-[#022c4f] text-sm w-36 mx-28">Esqueci minha senha</button>
+            <button className="flex flex-wrap items-center justify-center text-white text-lg bg-[#074173] hover:bg-[#022c4f] w-28 mx-32 py-2 rounded-md">Entrar</button>
           </div>
-          <div>
-            <p className="text-[#074173] pl-4 pb-1">Telefone</p>
-            <input type='textarea' placeholder='(00) 0 0000-0000' className='w-96 text-[#878787] rounded-md border border-gray-300 py-2 pl-4'></input>
-          </div>
-          <div>
-            <p className="text-[#074173] pl-4 pb-1">Senha</p>
-            <input type='textarea' placeholder='Informe uma senha' className='w-96 text-[#878787] rounded-md border border-gray-300 py-2 pl-4'></input>
-          </div>
-          <div>
-            <p className="text-[#074173] pl-4 pb-1">Confirme sua senha</p>
-            <input type='textarea' placeholder='Digite novamente sua senha' className='w-96 text-[#878787] rounded-md border border-gray-300 py-2 pl-4'></input>
-          </div>
-        </div>
-
-        <div className="flex justify-start">
-          <button>
-            Ja possui conta?
-          </button>
-
-          <button>
-            Cadastrar
-          </button>
-        </div>
+        <button className="text-start text-base text-[#074173] hover:text-[#022c4f] text-sm pt-5">Não possui cadastro? Cadastre-se agora!</button>
       </div>
     </div>
-    </div>
+  </div>
   );
 }
 
---
+++ src/components/cadastro-usuario.jsx
@@ -0,0 +1,70 @@
+import {Search} from 'lucide-react';
+
+export default function CadastroUsuario() {
+
+  return (
+  <div className="font-Inter">
+    <header className="h-[130px] bg-white drop-shadow-lg">
+      <div className="flex justify-between items-center">
+        <div className="pl-8">
+          <img src="./assets/logo.png" alt="logo prefeitura" className="h-[130px]"></img>
+        </div>
+
+        <div className="flex flex-row gap-10 pr-16">
+          <div class="bg-gray-300 w-24 h-7"></div>
+          <div class="bg-gray-300 w-24 h-7"></div>
+          <div class="bg-gray-300 w-24 h-7"></div>
+          <div class="bg-gray-300 w-24 h-7"></div>
+          <div class="bg-gray-300 w-24 h-7"></div>
+          <button>
+          <Search />
+          </button>
+        </div>
+      </div>
+    </header>
+
+    <div className="bg-slate-300 w-full flex flex-col justify-center items-center pt-5">
+      <div className="bg-white w-3/4 rounded-sm flex flex-col items-center">
+        <h1 className="text-center text-xl pt-7 pb-5 font-bold">Cadastre-se na Secretaria da Educação</h1>
+
+        <div className="grid grid-cols-2 gap-14 pl-20 pr-20 pb-10">
+          <div>
+            <p className="text-[#074173] pl-4 pb-1 text-lg">Nome Completo</p>
+            <input type='textarea' placeholder='Informe seu nome completo' className='w-96 text-[#878787] rounded-md border border-gray-500 py-2 pl-4'></input>
+          </div>
+          <div>
+            <p className="text-[#074173] pl-4 pb-1 text-lg">Matrícula</p>
+            <input type='textarea' placeholder='Formato (000000-0)' className='w-96 text-[#878787] rounded-md border border-gray-500 py-2 pl-4'></input>
+          </div>
+          <div>
+            <p className="text-[#074173] pl-4 pb-1 text-lg">E-mail</p>
+            <input type='textarea' placeholder='Informe seu e-mail' className='w-96 text-[#878787] rounded-md border border-gray-500 py-3 pl-4'></input>
+          </div>
+          <div>
+            <p className="text-[#074173] pl-4 pb-1 text-lg">Telefone</p>
+            <input type='textarea' placeholder='(00) 0 0000-0000' className='w-96 text-[#878787] rounded-md border border-gray-500 py-2 pl-4'></input>
+          </div>
+          <div>
+            <p className="text-[#074173] pl-4 pb-1 text-lg">Senha</p>
+            <input type='textarea' placeholder='Informe uma senha' className='w-96 text-[#878787] rounded-md border border-gray-500 py-2 pl-4'></input>
+          </div>
+          <div>
+            <p className="text-[#074173] pl-4 pb-1 text-lg">Confirme sua senha</p>
+            <input type='textarea' placeholder='Digite novamente sua senha' className='w-96 text-[#878787] rounded-md border border-gray-500 py-2 pl-4'></input>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white w-3/4 rounded-sm flex justify-between items-center drop-shadow-lg mb-5 px-20 pb-5">
+        <button className="text-[#074173] text-lg hover:text-[#022c4f]">
+          Já possui conta?
+        </button>
+
+        <button className="bg-[#074173] text-white px-5 py-2 rounded text-lg hover:bg-[#022c4f]">
+          Cadastrar
+        </button>
+      </div>
+    </div>
+  </div>
+  );
+}
