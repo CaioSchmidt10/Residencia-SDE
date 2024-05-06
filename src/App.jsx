f03fb2444d00e612146c3e71bc309962a939b979
Caio Schmidt <caioschmidtcdc@gmail.com>
2024-05-06 19:53:21 -0300

    Alterações da tela principal

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import './App.css';
 import {CircleUserRound} from 'lucide-react';
 import {LogOut} from 'lucide-react';
 import {SquareUserRound} from 'lucide-react';
+import {Home} from 'lucide-react';
+import {StickyNote} from 'lucide-react';
+import {ChevronDown} from 'lucide-react';
 
 function App() {
 
@@ -27,21 +30,35 @@ function App() {
     </header>
 
     <div className="grid grid-cols-2">
-      <div className="col-span-1 w-[330px] bg-[#10439F] text-white">
-        <div className="flex flex-row gap-3 px-5 py-3">
+      <div className="col-span-1 w-[330px] h-[33rem] bg-[#10439F] text-white">
+        <div className="flex flex-row gap-3 px-5 py-3 drop-shadow-lg">
           <button>
             <SquareUserRound  size={80}/>
           </button>
           <div>
-            <button className='text-lg pt-4'>
+            <button className='text-lg mt-4 hover:bg-gray-400 rounded hover:text-slate-600'>
               Nome do Fiscal
             </button>
             <p>Matricula: 000000</p>
           </div>
         </div>
         <div className="flex flex-col">
-          <button className="px-4 py-3 w-full hover:bg-[#042F54] text-start">
-            Tela de início
+          <button className="px-4 py-3 w-full hover:bg-[#042F54] text-lg text-start flex flex-row gap-2">
+            <Home />
+            <p>Tela de início</p>
+          </button>
+          <button className="px-4 py-3 w-full hover:bg-[#042F54] text-lg text-start flex flex-row gap-2">
+            <StickyNote />
+            <p>Gestão Contratual</p>
+            <div className="ml-20">
+              <ChevronDown />
+            </div>
+          </button>
+          <button className="px-4 py-3 w-full hover:bg-[#042F54] text-lg text-start flex flex-row gap-2">
+            <p>Lorem Ipsum</p>
+          </button>
+          <button className="px-4 py-3 w-full hover:bg-[#042F54] text-lg text-start flex flex-row gap-2">
+            <p>Lorem Ipsum</p>
           </button>
         </div>
       </div>
