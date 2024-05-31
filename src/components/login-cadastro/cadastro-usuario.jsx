1ab208dd5eaebcdf93f6f2b6e8304ca8f1688603
CaioSchmidt10 <caioschmidtcdc@gmail.com>
2024-05-31 15:47:16 -0300

    Responsividade Telas Login e Cadastro

--- src/components/login-cadastro/cadastro-usuario.jsx
+++ src/components/login-cadastro/cadastro-usuario.jsx
@@ -60,60 +60,77 @@ export default function CadastroUsuario() {
       </div>
     </header>
 
-    <div className="bg-slate-300 w-full flex flex-col justify-center items-center pt-5 xm:h-[100vh] ">
+    <div className="bg-slate-300 w-full flex flex-col justify-center items-center pt-5 xm:h-[100vh] md:h-[65.50rem] lg:h-[77rem] ">
       <div className="bg-white w-3/4 rounded-sm flex flex-col items-center">
-        <h1 className="text-center text-xl pt-7 pb-5 font-bold">Cadastre-se na Secretaria da Educação</h1>
+        <h1 className="text-center text-xl pt-7 pb-5 font-bold 
+        md:text-2xl lg:text-3xl">Cadastre-se na Secretaria da Educação</h1>
 
-        <div className="grid grid-cols-2 gap-14 px-20 pb-10 xm:grid-cols-1 xm:gap-5 xm:px-5 ">
+        <div className="grid grid-cols-2 gap-14 px-20 pb-10 
+        xm:grid-cols-1 xm:gap-5 xm:px-5 md:grid-cols-1 md:gap-10 lg:grid-cols-1 lg:gap-7">
           <div>
-            <p className="text-[#074173] pl-4 pb-1 text-lg">Nome Completo</p>
+            <p className="text-[#074173] pl-4 pb-1 text-lg 
+            md:text-xl md:font-bold lg:text-xl lg:font-bold">Nome Completo</p>
             <input 
             onChange={handleNameChange}
-            type='textarea' placeholder='Informe seu nome completo' className='w-96 text-[#878787] rounded-md border border-gray-500 py-2 pl-4 xm:w-64'></input>
+            type='textarea' placeholder='Informe seu nome completo' className='w-96 text-[#878787] rounded-md border border-gray-500 py-2 pl-4 
+            xm:w-64 md:h-[4rem] md:text-xl lg:h-[5rem] lg:text-xl'></input>
           </div>
           <div>
-            <p className="text-[#074173] pl-4 pb-1 text-lg">Matrícula</p>
+            <p className="text-[#074173] pl-4 pb-1 text-lg 
+            md:text-xl md:font-bold lg:text-xl lg:font-bold">Matrícula</p>
             <input
             onChange={handleMatriculaChange} 
-            type='textarea' placeholder='Formato (000000-0)' className='w-96 text-[#878787] rounded-md border border-gray-500 py-2 pl-4 xm:w-full'></input>
+            type='textarea' placeholder='Formato (000000-0)' className='w-96 text-[#878787] rounded-md border border-gray-500 py-2 pl-4 
+            xm:w-full md:h-[4rem] md:text-xl lg:h-[5rem] lg:text-xl'></input>
           </div>
           <div>
-            <p className="text-[#074173] pl-4 pb-1 text-lg">E-mail</p>
+            <p className="text-[#074173] pl-4 pb-1 text-lg 
+            md:text-xl md:font-bold lg:text-xl lg:font-bold">E-mail</p>
             <input 
             onChange={handleEmailChange}
-            type='email' placeholder='Informe seu e-mail' className='w-96 text-[#878787] rounded-md border border-gray-500 py-3 pl-4 xm:w-full'></input>
+            type='email' placeholder='Informe seu e-mail' className='w-96 text-[#878787] rounded-md border border-gray-500 py-3 pl-4 
+            xm:w-full md:h-[4rem] md:text-xl lg:h-[5rem] lg:text-xl'></input>
           </div>
           <div>
-            <p className="text-[#074173] pl-4 pb-1 text-lg">Telefone</p>
+            <p className="text-[#074173] pl-4 pb-1 text-lg 
+            md:text-xl md:font-bold lg:text-xl lg:font-bold">Telefone</p>
             <input 
             onChange={handleTelefoneChange}
-            type='textarea' placeholder='(00) 0 0000-0000' className='w-96 text-[#878787] rounded-md border border-gray-500 py-2 pl-4 xm:w-full'></input>
+            type='textarea' placeholder='(00) 0 0000-0000' className='w-96 text-[#878787] rounded-md border border-gray-500 py-2 pl-4 
+            xm:w-full md:h-[4rem] md:text-xl lg:h-[5rem] lg:text-xl'></input>
           </div>
           <div>
-            <p className="text-[#074173] pl-4 pb-1 text-lg">Senha</p>
+            <p className="text-[#074173] pl-4 pb-1 text-lg 
+            md:text-xl md:font-bold lg:text-xl lg:font-bold">Senha</p>
             <input 
             onChange={handleSenhaChange}
-            type='password' placeholder='Informe uma senha' className='w-96 text-[#878787] rounded-md border border-gray-500 py-2 pl-4 xm:w-full'></input>
+            type='password' placeholder='Informe uma senha' className='w-96 text-[#878787] rounded-md border border-gray-500 py-2 pl-4 
+            xm:w-full md:h-[4rem] md:text-xl lg:h-[5rem] lg:text-xl'></input>
           </div>
           <div>
-            <p className="text-[#074173] pl-4 pb-1 text-lg">Confirme sua senha</p>
+            <p className="text-[#074173] pl-4 pb-1 text-lg 
+            md:text-xl md:font-bold lg:text-xl lg:font-bold">Confirme sua senha</p>
             <input
             onChange={handleConfirmarSenhaChange}
-            type='password' placeholder='Digite novamente sua senha' className='w-96 text-[#878787] rounded-md border border-gray-500 py-2 pl-4 xm:w-full'></input>
+            type='password' placeholder='Digite novamente sua senha' className='w-96 text-[#878787] rounded-md border border-gray-500 py-2 pl-4 
+            xm:w-full md:h-[4rem] md:text-xl lg:h-[5rem] lg:text-xl'></input>
           </div>
         </div>
       </div>
 
-      <div className="bg-white w-3/4 rounded-sm flex justify-between items-center drop-shadow-lg mb-5 px-[145px] pb-[44px] xm:px-[40px] xm:gap-5 xm:mb-0">
+      <div className="bg-white w-3/4 rounded-sm flex justify-between items-center drop-shadow-lg mb-5 px-[145px] pb-[44px] 
+      xm:px-[40px] xm:gap-5 xm:mb-0 md:px-[100px]">
         <button 
         onClick={trocarTelaLogin}
-        className="text-[#074173] text-lg hover:text-[#022c4f] ">
+        className="text-[#074173] text-lg hover:text-[#022c4f] 
+        md:text-xl lg:text-2xl">
           Já possui conta?
         </button>
 
         <button 
         onClick={handleCadastro}
-        className="bg-[#074173] text-white px-5 py-2 rounded text-lg hover:bg-[#022c4f]">
+        className="bg-[#074173] text-white px-5 py-2 rounded text-lg hover:bg-[#022c4f] 
+        md:text-xl md:px-7 md:py-3 lg:text-2xl lg:px-10 lg:py-4">
           Cadastrar
         </button>
       </div>
--- src/components/login-cadastro/login-usuario.jsx
+++ src/components/login-cadastro/login-usuario.jsx
@@ -34,32 +34,41 @@ export default function LoginUsuario () {
       </div>
     </header>
 
-    <div className="bg-slate-300 w-full h-full flex flex-row gap-20 justify-center items-center pt-10 pb-[73px] xm:h-[85.50vh]">
+    <div className="bg-slate-300 w-full h-full flex flex-row gap-20 justify-center items-center pt-10 pb-[73px] 
+      xm:h-[85.50vh] md:h-[87.50vh] lg:h-[90.50vh]">
 
-      <div className= "w-[25rem] h-[29.5rem] bg-white drop-shadow-lg px-5 flex flex-col xm:w-[20rem] xm:h-[31rem]">
-          <h1 className="text-center pt-7 text-xl pb-7">Indentifique-se na Secretaria de Educação</h1>
-          <p className="text-[#074173] pl-8 text-lg xm:pl-3">E-mail ou Matricula</p>
+      <div className= "w-[25rem] h-[29.5rem] bg-white drop-shadow-lg px-5 flex flex-col xm:w-[20rem] xm:h-[31rem] md:w-[35rem] lg:w-[50rem] lg:h-[33rem]">
+          <h1 className="text-center pt-7 text-xl pb-7 
+          md:text-2xl lg:text-3xl">Indentifique-se na Secretaria de Educação</h1>
+          <p className="text-[#074173] pl-8 text-lg 
+          xm:pl-3 md:text-center md:font-bold md:pl-0 lg:text-center lg:pl-0 lg:text-xl lg:font-bold">E-mail ou Matricula</p>
           <div className="flex flex-wrap justify-center items-center pb-7">
             <input 
             onChange={handleEmailMatriculaChange}
-            type='textarea' placeholder='Digite seu e-mail ou matricula' className='max-w-full w-80 text-[#878787] rounded-md border border-gray-500 py-3 pl-4'></input>
+            type='textarea' placeholder='Digite seu e-mail ou matricula' className='max-w-full w-80 text-[#878787] rounded-md border border-gray-500 py-3 pl-4
+            md:text-xl lg:h-[5rem] lg:text-xl'></input>
           </div>
-          <p className="text-[#074173] pl-8 text-lg xm:pl-3">Senha</p>
+          <p className="text-[#074173] pl-8 text-lg 
+          xm:pl-3 md:text-center md:font-bold md:pl-0 lg:text-center lg:pl-0 lg:text-xl lg:font-bold">Senha</p>
           <div className="flex flex-wrap justify-center items-center">
             <input 
             onChange={handleSenhaChange}
-            type='password' placeholder='Digite sua senha' className='max-w-full w-80 text-[#878787] rounded-md border border-gray-500 py-3 pl-4'></input>
+            type='password' placeholder='Digite sua senha' className='max-w-full w-80 text-[#878787] rounded-md border border-gray-500 py-3 pl-4
+            md:text-xl lg:h-[5rem] lg:text-xl'></input>
           </div>
           <div className="flex flex-col content-center items-center justify-center gap-5">
-            <button className="text-center pt-2 text-[#074173] hover:text-[#022c4f] text-sm xm:font-bold">Esqueci minha senha</button>
+            <button className="text-center pt-2 text-[#074173] hover:text-[#022c4f] text-sm 
+            xm:font-bold md:text-base md:font-bold lg:text-xl lg:font-bold lg:pt-5">Esqueci minha senha</button>
             <button 
             onClick={trocarTelaPrincipal}
-            className="flex flex-wrap items-center justify-center text-white text-lg bg-[#074173] hover:bg-[#022c4f] w-28 py-2 rounded-md">
+            className="flex flex-wrap items-center justify-center text-white text-lg bg-[#074173] hover:bg-[#022c4f] w-28 py-2 rounded-md
+            lg:py-5 md:text-lg lg:text-xl lg:font-bold">
             Entrar</button>
           </div>
         <button 
         onClick={trocarTelaCadastro}
-        className="text-start text-[#074173] hover:text-[#022c4f] text-sm pt-5 xm:text-center xm:font-bold">Não possui cadastro? Cadastre-se agora!</button>
+        className="text-start text-[#074173] hover:text-[#022c4f] text-sm pt-5 
+        xm:text-center xm:font-bold md:text-lg lg:font-bold lg:text-xl">Não possui cadastro? Cadastre-se agora!</button>
       </div>
     </div>
   </div>
